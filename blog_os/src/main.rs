f8cf0466c4bf3ce194d51deb15db1ff4f8cb9b95
AMD <michojekunle1@gmail.com>
2025-11-17 22:58:16 +0100

    linker error

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -1,5 +1,6 @@
 
 #![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
@@ -8,5 +9,7 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-
-// #[no_mangle]
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
