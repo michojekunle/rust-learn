42344f33c28a0b0a119a16410b2851156dc1c47d
AMD <michojekunle1@gmail.com>
2025-11-21 18:18:09 +0100

    .

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -3,15 +3,15 @@
 
 use core::panic::PanicInfo;
 
+// This function is called on panic 
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
 #[unsafe(no_mangle)] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
-    loop {}
-}
-
-// This function is called on panic 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
