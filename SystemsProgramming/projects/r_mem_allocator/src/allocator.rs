82321d6a12d4aaff0862a3625ebdc0d2894f7358
AMD <michojekunle1@gmail.com>
2026-09-24 14:19:13 +0100

    feat: implement memory allocator with bump and linked list strategies

---
+++ SystemsProgramming/projects/r_mem_allocator/src/allocator.rs
@@ -0,0 +1,37 @@
+pub mod bump;
+pub mod linked_list;
+use std::sync::{Mutex, MutexGuard};
+
+use bump::BumpAllocator;
+
+#[global_allocator]
+static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+
+pub struct Locked<A> {
+    inner: Mutex<A>,
+}
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> MutexGuard<'_, A> {
+        self.inner.lock().unwrap()
+    }
+}
+
+fn align_up(addr: usize, align: usize) -> usize {
+    // let remainder = addr % align;
+
+    // if remainder == 0 {
+    //     addr // addr already aligned
+    // } else {
+    //     addr - remainder + align
+    // }
+
+    // The above is better implemented thus leveraging that align value will always be a power of two coming from GlobalAlloc
+    (addr + align - 1) & !(align - 1)
+}
---
+++ SystemsProgramming/projects/r_mem_allocator/src/allocator/linked_list.rs
@@ -0,0 +1,66 @@
+use super::align_up;
+use core::mem;
+
+struct ListNode {
+    size: usize,
+    next: Option<&'static mut ListNode>,
+}
+
+impl ListNode {
+    const fn new(size: usize) -> Self {
+        ListNode { size, next: None }
+    }
+
+    fn start_addr(&self) -> usize {
+        self as *const Self as usize
+    }
+
+    fn end_addr(&self) -> usize {
+        self.start_addr() + self.size
+    }
+}
+
+pub struct LinkedListAllocator {
+    head: ListNode,
+}
+
+impl LinkedListAllocator {
+    ///Creates an entpy LinkedListAllocator.
+    pub const fn new() -> Self {
+        Self {
+            head: ListNode::new(0),
+        }
+    }
+
+    /// Initializes the allocator with the given heap bounds.
+    ///
+    /// This fuction is unsafe becuase the caller must guarantee that the given
+    /// heap bounds are valid and that the heap is unused. This method must be
+    /// called only once
+    pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
+        unsafe {
+            self.add_free_region(heap_start, heap_size);
+        }
+    }
+
+    /// Adds the given memory region to the front of the list.
+    unsafe fn add_free_region(&mut self, addr: usize, size: usize) {
+        // ensure that the freed region is capable of holding ListNode
+        assert_eq!(align_up(addr, mem::align_of::<ListNode>()), addr);
+        assert!(size >= mem::size_of::<ListNode>());
+
+        // creatw a new list node and apend it at the start of the list
+        let mut node = ListNode::new(size);
+        node.next = self.head.next.take();
+        let node_ptr = addr as *mut ListNode;
+        unsafe {
+            node_ptr.write(node);
+            self.head.next = Some(&mut *node_ptr)
+        }
+    }
+}
+
+https://job-boards.greenhouse.io/k2spacecorporation/jobs/5165724008
+
+"https://www.comeet.com/jobs/metalbear/8A.002/backend-software-engineer-rust-fully-remote/1C.176"
+
